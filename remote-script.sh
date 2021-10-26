7f00503fb0e842b1e36ef0a9ec019642996f4c1d
Vladimir Taran <7759160-ognivo777@users.noreply.gitlab.com>
2021-10-26 23:00:19 +0000

    Update remote-script.sh

--- remote-script.sh
+++ remote-script.sh
@@ -1,2 +1,12 @@
 uname -a
 sudo dnf update -y
+sudo dnf module list postgresql
+sudo dnf -y module enable postgresql:13
+sudo dnf -y install postgresql-server
+sudo postgresql-setup --initdb
+sudo systemctl enable postgresql
+sudo systemctl start postgresql
+sudo -i -u postgres
+createdb pegape86
+exit
+exit
