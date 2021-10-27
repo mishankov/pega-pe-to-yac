0ed83512a383c2fb5f0cb2705708495daf375814
ognivo777 <ognivo777@gmail.com>
2021-10-27 04:09:44 +0300

    tomcat install update

--- tomcat-install.sh
+++ tomcat-install.sh
@@ -5,16 +5,21 @@ wget http://dlcdn.apache.org/tomcat/tomcat-8/v8.5.72/bin/apache-tomcat-8.5.72.ta
 
 sudo mkdir /opt/tomcat
 sudo mkdir /opt/pegatemp
-
-cd /opt/tomcat
+sudo tar xvf apache-tomcat-*tar.gz -C /opt/tomcat --strip-components=1
 
 sudo groupadd tomcat
 sudo useradd -M -s /bin/nologin -g tomcat -d /opt/tomcat tomcat
-sudo chgrp -R tomcat /opt/tomcat
-sudo chgrp -R tomcat /opt/pegatemp
 
-sudo chmod -R g+r conf
-sudo chmod g+x conf
-sudo chown -R tomcat webapps/ work/ temp/ logs/ conf/
+sudo chgrp -R tomcat /opt/tomcat
+sudo chown -R tomcat /opt/tomcat
+sudo chgrp -R tomcat /opt/pegatemp
 sudo chown -R tomcat /opt/pegatemp
+
+sudo cp tomcat.service /etc/systemd/system/tomcat.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable tomcat
+sudo systemctl start tomcat
+sudo systemctl status tomcat
+
 exit
