e9546de2c0b6bb1a44478424673558a87193b190
ognivo777 <ognivo777@gmail.com>
2021-10-27 05:44:56 +0300

    upload

--- before.sh
+++ before.sh
@@ -3,6 +3,4 @@ sudo dnf update -y
 sudo dnf -y install wget tar nano
 wget https://gist.githubusercontent.com/cronfy/00e23e126d4fbf3fab37392e414f0e4c/raw/a6c7c8f4ea2462483dc7159dbf42c2ebdaa072e9/dl-cloud-mail-ru.sh
 chmod +x dl-cloud-mail-ru.sh
-./dl-cloud-mail-ru.sh https://cloud.mail.ru/public/XF1o/onAr98fwY postgresql-42.0.0.jar
-./dl-cloud-mail-ru.sh https://cloud.mail.ru/public/JyeH/Z9Kznb5pD prhelp.war 
-./dl-cloud-mail-ru.sh https://cloud.mail.ru/public/gvvx/v1KdgbnXb prweb.war
+
--- tomcat-install.sh
+++ tomcat-install.sh
@@ -1,12 +1,6 @@
 sudo dnf -y install java-11-openjdk-headless
-sudo dnf -y install wget tar nano
 
 wget http://dlcdn.apache.org/tomcat/tomcat-8/v8.5.72/bin/apache-tomcat-8.5.72.tar.gz
-wget https://gist.githubusercontent.com/cronfy/00e23e126d4fbf3fab37392e414f0e4c/raw/a6c7c8f4ea2462483dc7159dbf42c2ebdaa072e9/dl-cloud-mail-ru.sh
-chmod +x dl-cloud-mail-ru.sh
-./dl-cloud-mail-ru.sh https://cloud.mail.ru/public/XF1o/onAr98fwY postgresql-42.0.0.jar
-./dl-cloud-mail-ru.sh https://cloud.mail.ru/public/JyeH/Z9Kznb5pD prhelp.war 
-./dl-cloud-mail-ru.sh https://cloud.mail.ru/public/gvvx/v1KdgbnXb prweb.war
 
 sudo mkdir /opt/tomcat
 sudo mkdir /opt/pegatemp
