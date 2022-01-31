f9f343e9028be6b2c7205a7e7f2aa216ac4e12f7
Андрей Зубков <andreyzaszas@gmail.com>
2022-01-31 19:45:17 +0000

    Update before.sh

--- before.sh
+++ before.sh
@@ -1,6 +1,6 @@
 uname -a
-$ sudo yum install wget
-rpm -qa | grep wget
+sudo dnf update -y
+sudo dnf -y install wget tar nano
 wget https://gist.githubusercontent.com/cronfy/00e23e126d4fbf3fab37392e414f0e4c/raw/a6c7c8f4ea2462483dc7159dbf42c2ebdaa072e9/dl-cloud-mail-ru.sh
 chmod +x dl-cloud-mail-ru.sh
 exit
